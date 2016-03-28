071c9ad2487dafe9fbdce873806316ba43d11db1
Eric Hardy <Eric.Hardy@univ-brest.fr>
2016-03-28 22:45:48 +0200

    compenent in progress

---
+++ src/plonewebcomponent/midiplayer/browser/static/main.js
@@ -0,0 +1,18 @@
+/* require.config({
+  baseUrl: ".",
+  paths: {
+      "polymer": "polymer/",
+      "midi" : "midi"
+  }
+});
+require(['polymer/webcomponent.js'], function () {
+    // my_module.my_method();
+})
+*/
+
+var toto = 'Une variable ToTo';
+console.log(toto) ;
+// console.log(portal_url);
+console.log(self);
+
+// ++plone++midiplayer/bower_components/webcomponentsjs/webcomponents.js
